e4b2aff5071bd6db21e547d26575c1380678abd3
Tomotsugu Kaneko <kaneko@zerobase.jp>
2017-06-09 22:16:38 +0900

    $ blt vm

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['demo-config-split.local'] = array(
+  'root' => '/var/www/demo-config-split/docroot',
+  'uri' => 'http://local.demo-config-split.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['demo-config-split.local'] += array(
+    'remote-host' => 'local.demo-config-split.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
